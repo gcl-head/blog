d29fdc514d32e8421fdd3340f53925d3f233637b
gcl <1132067974@qq.com>
2019-09-15 11:56:14 +0800

    修改axios baseurl

--- frontend/src/plugin/axios/index.js
+++ frontend/src/plugin/axios/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // 创建一个 axios 实例
 const service = axios.create({
-  baseURL: 'http://127.0.0.1:8000/',
+  baseURL: 'http://139.155.27.69:8000/',
   timeout: 5000 // 请求超时时间
   // headers: {
   //   'Content-Type': 'application/x-www-form-urlencoded'
