d105ec5bca20c5288e9b524fa3b91e10cbb3c5c0
gcl <1132067974@qq.com>
2019-09-16 19:41:10 +0800

    优化时间颜色&&上传新博客

--- frontend/src/views/pages/blog/index.vue
+++ frontend/src/views/pages/blog/index.vue
@@ -51,8 +51,8 @@ export default {
         .then(res => {
           that.markdownRefresh = false
           that.compiledMarkdown = '>' +
-            '创建时间:' + res.data.create_timestamp + '\n' +
-            '>最后修改时间:' + res.data.last_edit_timestamp + '\n' +
+            '<font color="#808080">创建时间:' + res.data.create_timestamp + '\n' +
+            '>最后修改时间:' + res.data.last_edit_timestamp + '</font>\n' +
             '---\n' + res.data.text
           that.$nextTick(function () {
             that.markdownRefresh = true
