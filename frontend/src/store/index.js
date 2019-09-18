342b79063a6dbe81a65eb8d02a3bcad797fd1c9b
gcl <1132067974@qq.com>
2019-09-18 19:16:59 +0800

    store文件优化

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import Mary from './modules/Mary'
-
 Vue.use(Vuex)
+const files = require.context('./modules', false, /\.js$/)
+const modules = {}
+
+files.keys().forEach(key => {
+  modules[key.replace(/(\.\/|\.js)/g, '')] = files(key).default
+})
 
 export default new Vuex.Store({
   modules: {
-    Mary
+    modules
   }
 })
