d8f3b45416b125e8559268b7c32bf93c90c6e305
gcl <1132067974@qq.com>
2019-09-29 18:35:53 +0800

    改axios

--- frontend/src/plugin/axios/index.js
+++ frontend/src/plugin/axios/index.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 // 创建一个 axios 实例
 const service = axios.create({
-  // baseURL: 'bighead.net.cn',
-  baseURL: 'http://127.0.0.1:8000/',
+  baseURL: 'bighead.net.cn',
+  // baseURL: 'http://127.0.0.1:8000/',
   timeout: 5000 // 请求超时时间
   // headers: {
   //   'Content-Type': 'application/x-www-form-urlencoded'
