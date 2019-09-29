76a6c6c943ce97898aff0092542c271560c377e3
gcl <1132067974@qq.com>
2019-09-29 17:11:41 +0800

    改变assetsPublicPath项目更目录

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/blog',
     proxyTable: {},
 
     // Various Dev Server settings
