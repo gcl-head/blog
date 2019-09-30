fbb3fdc295576eb4e53ab1550d07e2cb7e379e1e
gcl <1132067974@qq.com>
2019-09-30 19:31:27 +0800

    手机端侧边栏兼容调整

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -10,6 +10,7 @@ export default {
   name: 'App',
   created () {
     if (navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)) {
+      console.log('change')
       this.$refs.bar.changeCollapse()
     }
   }
