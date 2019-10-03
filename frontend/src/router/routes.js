f3619f11cdca0bdb295280ae8666b4afe62d863e
gcl <1132067974@qq.com>
2019-10-03 11:26:51 +0800

    loading and error

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -2,9 +2,31 @@
 // import blog from '@/views/pages/blog'
 // import index from '@/views/pages/index'
 // 简化加载路径
-const nav = () => import('@/components/navigation-bar')
-const blog = () => import('@/views/pages/blog')
-const index = () => import('@/views/pages/index')
+function lazyLoadView (AsyncView) {
+  const AsyncHandler = () => ({
+    // 需要加载的组件 (应该是一个 `Promise` 对象)
+    component: AsyncView,
+    // 异步组件加载时使用的组件
+    loading: require('@/components/public/RouteLoading.vue').default,
+    // 加载失败时使用的组件
+    error: require('@/components/public/RouteError.vue').default,
+    // 展示加载时组件的延时时间。默认值是 200 (毫秒)
+    delay: 200,
+    // 如果提供了超时时间且组件加载也超时了，
+    // 则使用加载失败时使用的组件。默认值是：`Infinity`
+    timeout: 10000
+  })
+  return Promise.resolve({
+    functional: true,
+    render (h, { data, children }) {
+      return h(AsyncHandler, data, children)
+    }
+  })
+}
+
+const nav = () => lazyLoadView(import('@/components/navigation-bar'))
+const blog = () => lazyLoadView(import('@/views/pages/blog'))
+const index = () => lazyLoadView(import('@/views/pages/index'))
 // 主框架
 const frameIn = [
   {
