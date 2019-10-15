82eddf50435b67487a58aa0ae0144b30cd695bb8
gcl <1132067974@qq.com>
2019-10-16 07:16:54 +0800

    更改admin操作页，增加搜索栏

--- backend/admin.py
+++ backend/admin.py
@@ -7,7 +7,7 @@ from backend import models
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('href', 'title', 'name')   # 指定要显示的字段
     search_fields = ('name',)               # 指定要搜索的字段，将会出现一个搜索框让管理员搜索关键词
-    fields = ('href', 'title', 'name')    # 自定义编辑表单，在编辑表单的时候 显示哪些字段，显示的属性
+    # fields = ('href', 'title', 'name')    # 自定义编辑表单，在编辑表单的时候 显示哪些字段，显示的属性
 
 
 admin.site.register(models.BlogItem, AuthorAdmin)
