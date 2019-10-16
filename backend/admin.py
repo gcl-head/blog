c413a6679592ba97cf020779d95c8b040bf20897
gcl <1132067974@qq.com>
2019-10-16 08:05:55 +0800

    博客更新

--- backend/admin.py
+++ backend/admin.py
@@ -5,7 +5,7 @@ from backend import models
 
 
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('href', 'title', 'name')   # 指定要显示的字段
+    list_display = ('id', 'href', 'title', 'name')   # 指定要显示的字段
     search_fields = ('name',)               # 指定要搜索的字段，将会出现一个搜索框让管理员搜索关键词
     # fields = ('href', 'title', 'name')    # 自定义编辑表单，在编辑表单的时候 显示哪些字段，显示的属性
 
